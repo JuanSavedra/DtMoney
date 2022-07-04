8de5b57333b6b206375a62789846cc96e97d4fa9
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-07-04 14:07:24 -0300

    new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components /Header";
 import { useState } from 'react';
 import { Dashboard } from './components /Dashboard/index';
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components /NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -21,12 +22,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-      <Modal 
+      <NewTransactionModal 
         isOpen={isOpenNewTransactionModal}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
